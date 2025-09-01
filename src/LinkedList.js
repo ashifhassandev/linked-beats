e05880206cc44696de31cbba3541ebc16313c973
Ashif Hassan <ashifhassan1996@gmail.com>
2025-09-01 10:51:14 +0530

    add functions to add, remove, shuffle songs

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -34,4 +34,71 @@ export class Playlist {
     this.current = this.head;
     return this.current.song;
   }
+
+  nextSong(loop = false) {
+    if (this.current && this.current.next) {
+      this.current = this.current.next;
+      return this.current.song;
+    } else if (loop && this.head) {
+      this.current = this.head;
+      return this.current.song;
+    }
+
+    return null;
+  }
+
+  removeSong(title) {
+    if (!this.head) return;
+
+    if (this.head.song.title === title) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next) {
+      if (current.song.title === title) {
+        current.next = current.next.next;
+        return;
+      }
+
+      current = current.next;
+    }
+  }
+
+  getAllSongs() {
+    let current = this.head;
+    const songs = [];
+
+    while (current) {
+      songs.push(current.song);
+      current = current.next;
+    }
+
+    return songs;
+  }
+
+  getRandomSong(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count === index) return current;
+      current = current.next;
+      count++;
+    }
+
+    return null;
+  }
+
+  shuffle() {
+    const songs = this.getAllSongs();
+
+    if (songs.length === 0) return null;
+
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    this.current = this.getRandomSong(randomIndex);
+    return this.current.song;
+  }
 }
